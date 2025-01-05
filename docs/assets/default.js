0ea3a1d9b37e3092bc51d2038b33b7a56cbee86d
Jess <NaturalBornCyborg@proton.me>
2025-01-05 04:34:21 -0500

    Delete the file at 'docs/assets/default.js'

--- docs/assets/default.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function () {
-	document.querySelector('.header-nav').classList.toggle('show');
-});
-
-document.getElementById('currentYear').textContent = new Date().getFullYear();
