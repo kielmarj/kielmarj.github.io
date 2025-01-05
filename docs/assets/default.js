4defe27189e3798384787cda92318806557f3696
Jess <NaturalBornCyborg@proton.me>
2025-01-05 04:25:44 -0500

    Update default.js

--- docs/assets/default.js
+++ docs/assets/default.js
@@ -1,2 +1,6 @@
+// Nav menu
+document.querySelector('.hamburger').addEventListener('click', function () {	document.querySelector('.header-nav').classList.toggle('show');
+});
+
 // Retrieves current year for footer
-document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById('currentYear').textContent = new Date().getFullYear();
